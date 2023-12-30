2b8096c60b47ef26d134d3623b53b162029adb50
ajaySankapal <ajay.sankapal@tensorgo.com>
2023-12-30 20:30:57 +0530

    image issue fixed_

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -9,7 +9,7 @@ const Product = ({ product }) => {
   return (
     <Card className='my-3 p-3 rounded'>
       <Link to={`/product/${product._id}`}>
-        <Card.Img src={`http://3.90.218.168:5000/${product.image}`} variant='top' />
+        <Card.Img src={`http://3.90.218.168:5000${product.image}`} variant='top' />
       </Link>
       <Card.Body>
         <Link to={`/product/${product._id}`}>
