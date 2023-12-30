a954923427083950dbc08ac5e4047cc72e0c025a
ajaySankapal <ajay.sankapal@tensorgo.com>
2023-12-30 19:59:09 +0530

    cors issue fixed

--- backend/server.js
+++ backend/server.js
@@ -9,6 +9,7 @@ import userRoutes from './routes/userRoutes.js'
 import orderRoutes from './routes/orderRoutes.js'
 import uploadRoutes from './routes/uploadRoutes.js'
 import { notFound, errorHandler } from './middleware/errorMiddleware.js'
+import cors from 'cors'
 import morgan from 'morgan'
 
 dotenv.config()
@@ -17,6 +18,7 @@ connectDB()
 //to accept the json data in the body
 const app = express()
 app.use(express.json())
+app.use(cors())
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'))
 }
