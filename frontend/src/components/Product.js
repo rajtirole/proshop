3568cf9939b7826998dd3b4b481ca7654c57a47d
ajaySankapal <ajay.sankapal@tensorgo.com>
2023-12-30 20:27:54 +0530

    image issue fixed

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -5,10 +5,11 @@ import { Link } from 'react-router-dom'
 import Rating from './Rating'
 
 const Product = ({ product }) => {
+  console.log(product.image, 'PRODUCT IMAGE')
   return (
     <Card className='my-3 p-3 rounded'>
       <Link to={`/product/${product._id}`}>
-        <Card.Img src={product.image} variant='top' />
+        <Card.Img src={`http://3.90.218.168:5000/${product.image}`} variant='top' />
       </Link>
       <Card.Body>
         <Link to={`/product/${product._id}`}>
